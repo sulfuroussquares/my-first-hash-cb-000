f76d17e147df0da70349cc5d9b101fb883e08df3
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-10 01:44:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,18 +17,14 @@ old_fashioned_things = {
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = shipping_manifest["oil_paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
 
 def adding
-  shipping_manifest = shipping_manifest["oil_paintings"]
+  
 
   # add 2 muskets to the shipping_manifest hash below
 
