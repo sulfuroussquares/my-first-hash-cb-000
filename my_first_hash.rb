4faac545996bd332b65ab903538d52bf6ebb0d18
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-10 01:46:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,16 +28,15 @@ shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest["oil_paintings"]
 
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
